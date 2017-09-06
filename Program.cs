156d449a45b442322c4a2872ed76429b567f395f
miyake <wmiyake@gmail.com>
2017-09-06 19:08:44 -0300

    Commit Segundo

--- Program.cs
+++ Program.cs
@@ -19,15 +19,20 @@ namespace ValueTypeVsReferenceType
 
             Teste teste1 = new Teste();
             teste1.Quantidade = 3;
+            teste1.testePai = new Teste();
+            teste1.testePai.Quantidade = 6;
             Teste teste2 = teste1;
             teste1.Quantidade = 5;
             Console.WriteLine("teste1.Quantidade = {0}, teste2.Quantidade = {1}", teste1.Quantidade, teste2.Quantidade);
+            Console.WriteLine("Quantidade do teste Pai é {0}", teste1.testePai.Quantidade);
             Console.ReadKey();
+            
         }
     }
 
     class Teste
     {
         public int Quantidade;
+        public Teste testePai;
     }
 }
